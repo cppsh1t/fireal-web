ebe596b8d330e8af1dacfe22c7f0e60b8ccc7ec2
bitten <1730648559@qq.com>
2024-04-28 14:53:35 +0800

    feat: developing basic functionalities underway.

---
+++ src/main/java/com/fireal/web/anno/RequestParamType.java
@@ -0,0 +1,10 @@
+package com.fireal.web.anno;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestParamType {
+    
+}
